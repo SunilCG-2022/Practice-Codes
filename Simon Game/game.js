39aaa452fa969c070ea0d9adaafa8ca1150b53e3
SunilCG-2022 <sunilcg2022@gmail.com>
2024-01-25 11:56:59 +0530

    QR code generator for website inputs.

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -6,7 +6,7 @@ var level = 0;
 
 $("document").keypress( function() {
     if(!started) {
-        $("#level-title").text("level" + level);
+        $("#level-title").text("Level" + level);
         nextSequence();
         started = true;
     }
@@ -15,16 +15,41 @@ $("document").keypress( function() {
 $(".btn").click(function() {
     var userChoosenColour = $(this).attr("id");
     playSound(userChoosenColour);
+    checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("Success");
+
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function() {
+                nextSequence();
+            },1000);
+        }
+    }else {
+        console.log("Wrong")
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        },200);
+
+        $("#level-title").text("Game Over, Press Any Key to start")
+    }
+}
+
 function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level" + level);
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    userClickedPattern.push(userChoosenColour);
-
+ 
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    
+    playSound(randomChosenColour);
 }
 
 function playSound(name) {
@@ -36,6 +61,6 @@ function animatePress(currentColour) {
     $("#" + currentColour).addClass("pressed");
 
     setTimeout(function() {
-        $("#" + currentColour).removeClass("pressed")
+        $("#" + currentColour).removeClass("pressed");
     },100);
 }
